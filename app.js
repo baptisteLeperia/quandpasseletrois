b5e73124716d6f07ad37ae545547404131367baf
Baptiste Lé-Peria <50098235+baptisteLeperia@users.noreply.github.com>
2023-01-31 08:46:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,120 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", async function (_e) {
-  document.onresize = () => {
-    document.body.style.height = window.innerHeight + "px";
-  };
+/**
+ * @typedef {Object} configItem
+ * @property {int}      beginTime                  - When the config should start to be active
+ * @property {int}      endTime                    - When the config should stop to be active
+ * @property {string}   stop                       - Bus stop where to check the buses
+ * @property {Object}   destination                - Possible destinations for the bus
+ * @property {string}   destination.numLignePublic - Number as sent from the API
+ * @property {boolean}  destination.sensAller      - Direction as sent from the API
+ * @property {string}   destination.label          - Name as shown in the UI
+ */
 
-  document.onresize(null);
+/**
+ * @var {configItem[]} config
+ */
+const config = [
+	{
+		beginTime: 0,
+		endTime: 12,
+		stop: "Isenbart",
+		destination: { numLignePublic: "L3", sensAller: false, label: "Temis" },
+	},
+	{
+		beginTime: 12,
+		endTime: 24,
+		stop: "Crous-Université",
+		destination: {
+			numLignePublic: "L3",
+			sensAller: true,
+			label: "Centre - Ville",
+		},
+	},
+];
 
-  const url =
-    "https://api.ginko.voyage/TR/getTempsLieu.do?idArret=CEPARGN2&nb=3";
+document.addEventListener("DOMContentLoaded", async function () {
+	/**
+	 * @returns {configItem}
+	 */
+	function getConfigFromTime() {
+		const today = new Date();
+		const now = today.getHours() + today.getMinutes() / 100;
 
-  const getTimes = async () => {
-    const response = await fetch(
-      "https://api.ginko.voyage/TR/getTempsLieu.do?idArret=CEPARGN2&nb=3"
-    );
+		for (const conf of config) {
+			if (conf.beginTime <= now && now < conf.endTime) {
+				return conf;
+			}
+		}
 
-    const body = await response.json();
+		throw Error("Le moment actuel n'est pas connu dans la config");
+	}
 
-    /** @type {string[]} times */
-    const times = body.objets.listeTemps.map((o) => o.temps);
+	/**
+	 * @param {string}   busStop
+	 * @param {string}   busLine
+	 * @param {object}   busDestination
+	 * @returns {Promise<string[]>}
+	 */
+	async function getTimesForLine(busStop, busDestination) {
+		// Oui, j’ai mis la clé API ici, parce que je m’en fiche et que j’espère que personne ne lira ce code de toutes façons
+		const response = await fetch(
+			`https://api.ginko.voyage/TR/getTempsLieu.do?nom=${busStop}&apiKey=lq9qK7DY9EjGC9ooPsP8BVuX5d8z&nb=3`
+		);
 
-    if (times.length === 0) {
-      document.getElementById("next").innerText = "Plus de bus aujourd'hui...";
-      return;
-    }
+		const body = await response.json();
 
-    document.getElementById("next").innerText = times.shift();
+		const times = body.objets.listeTemps
+			.filter((o) => busDestination.numLignePublic == o.numLignePublic)
+			.filter((o) => busDestination.sensAller == o.sensAller)
+			.map((o) => o.temps);
+		return times;
+	}
 
-    document.getElementById("others").innerHTML = times
-      .map((time) => `<li>${time}</li>`)
-      .join("");
-  };
+	/**
+	 * Fills the DOM
+	 *
+	 * @param {string}   busStop
+	 * @param {string}   busLine
+	 * @param {Object[]} busDestination
+	 * @param {string[]} times
+	 * @returns
+	 */
+	function putResult(busStop, busDestination, times) {
+		document.getElementById("bus-line").innerText =
+			busDestination.numLignePublic;
+		document.getElementById("bus-stop").innerText = busStop;
+		document.getElementById("bus-destination").innerText = busDestination.label;
 
-  setInterval(() => {
-    try {
-      getTimes();
-    } catch (e) {
-      document.getElementById("error").innerText = "T'as pas de réseau bolosse";
-    }
-  }, 500);
+		if (!times) {
+			document.getElementById("times").innerText = "Plus de bus aujourd'hui...";
+			return;
+		}
+
+		document.getElementById("times").innerHTML = times
+			.map((time) => `<li>${time}</li>`)
+			.join("");
+	}
+
+	async function findAndShowResults() {
+		const conf = getConfigFromTime();
+		const times = await getTimesForLine(conf.stop, conf.destination);
+		putResult(conf.stop, conf.destination, times);
+	}
+
+	try {
+		await findAndShowResults();
+		setInterval(findAndShowResults, 60 * 1000);
+	} catch (e) {
+		console.error(e);
+
+		const errorDiv = document.getElementById("error");
+
+		if (e.message.includes("NetworkError")) {
+			errorDiv.innerHTML = `<p>T’as pas de réseau bolosse</p><pre>${e}</pre>`;
+			return;
+		}
+
+		errorDiv.innerHTML = `<p>Erreur dans le JavaScript, dommage</p><pre>${e}</pre>`;
+	}
 });
